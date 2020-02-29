2d8f829636f88d808f61a7888c26b1deca107d65
Justkim <kimiya.saadat97@gmail.com>
2020-02-29 18:46:10 +0330

    removed -1 based on other impl

--- utils.py
+++ utils.py
@@ -13,24 +13,20 @@ class RunningStdMean():
         count_b=input_batch_b.shape[0]
         self.var,self.mean = self.parallel_variance(self.mean,self.count,self.var,mean_b,count_b,var_b)
         self.count = count_b + self.count
+
     def update_from_mean_std(self,mean_b,var_b,count_b):
         self.var, self.mean = self.parallel_variance(self.mean, self.count, self.var, mean_b, count_b, var_b)
         self.count = count_b + self.count
 
 
     def parallel_variance(self,avg_a, count_a, var_a, avg_b, count_b, var_b):
-        # print("COUNTA",count_a)
-        # print("COUNTB",count_b)
-        # print("avg a",avg_a)
-        # print("avg b",avg_b)
-        # print("vara",var_a)
-        # print("varb",var_b)
+
         delta = avg_b - avg_a
         m_a = var_a * (count_a)
         m_b = var_b * (count_b)
         M2 = m_a + m_b + np.square(delta) * count_a * count_b / (count_a + count_b)
         new_mean = avg_a + delta * count_b / (count_a + count_b)
-        new_var = (M2 / (count_a + count_b - 1))
+        new_var = M2 / (count_a + count_b)
         return new_var, new_mean
 
 
